588b1288cadfb0bfdd240634428e58da54c31f03
ruben.solozabal <rubensolozabal@gmail.com>
2026-01-18 00:00:52 +0400

    Add: dpss support

--- src/models/hippo/hippo.py
+++ src/models/hippo/hippo.py
@@ -49,6 +49,10 @@ def create_safari_frame(measure, N):
     elif measure.startswith("safari_fbsp"):
         F = addf.fbsp_frame(N, L, make_tight=False)
 
+    elif measure.startswith("safari_dpss"):
+        L=2**10  # Use smaller L for DPSS to avoid numerical issues
+        F = addf.dpss_frame(N, L, make_tight=False)
+
     else:
         raise NotImplementedError(f"Frame initialization {measure} not implemented.")
 
